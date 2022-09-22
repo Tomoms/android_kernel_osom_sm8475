154ccb6db828ce8c260f658d50df81bbcc68ef94
Rick Yiu <rickyiu@google.com>
2022-09-22 07:18:33 +0000

    ANDROID: cgroup: Add vendor hook for rebuild_root_domains_bypass
    Vendor could decide to bypass this function.
    
    Bug: 238390134
    Change-Id: Ia6bc71c7569d21da9ae226e5d8739f97b9ca1a4f
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -421,6 +421,9 @@ DECLARE_HOOK(android_vh_cpu_cgroup_css_free,
 	TP_PROTO(struct cgroup_subsys_state *css),
 	TP_ARGS(css));
 
+DECLARE_HOOK(android_vh_rebuild_root_domains_bypass,
+	TP_PROTO(bool cpuhp_tasks_frozen, bool *bypass),
+	TP_ARGS(cpuhp_tasks_frozen, bypass));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
