343ca46ff56f7aa6e0e79098f8ca6991e08b19e3
Juhyung Park <qkrwngud825@gmail.com>
2025-04-11 03:26:48 -0700

    Revert "kgsl: Restore original task priority after real-time elevation"
    This reverts commit 85ae52a386992f0674c92eee357e166edae1a17d.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -592,7 +592,6 @@ static int sendcmd(struct adreno_device *adreno_dev,
 	int ret;
 	struct submission_info info = {0};
 	int is_current_rt = rt_task(current);
-	int nice = task_nice(current);
 
 	mutex_lock(&device->mutex);
 
@@ -716,7 +715,7 @@ static int sendcmd(struct adreno_device *adreno_dev,
 		context->priority, drawobj->flags);
 
 	if (!is_current_rt)
-		sched_set_normal(current, nice);
+		sched_set_normal(current, 0);
 
 	mutex_unlock(&device->mutex);
 
@@ -746,7 +745,7 @@ static int sendcmd(struct adreno_device *adreno_dev,
 	return 0;
 err:
 	if (!is_current_rt)
-		sched_set_normal(current, nice);
+		sched_set_normal(current, 0);
 	mutex_unlock(&device->mutex);
 	return ret;
 }
