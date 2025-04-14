1eace8624b39fdc8102603a06f53ae30ec4ede80
Greg Kroah-Hartman <gregkh@google.com>
2025-04-14 14:21:17 +0000

    Revert "can: statistics: use atomic access in hot path"
    This reverts commit 9d2578886070b365089e47d4845ec82c2377e0aa which is
    commit 80b5f90158d1364cbd80ad82852a757fc0692bf2 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I90f0281d9af75348e0491947d77774192f22fd1a
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/can/af_can.c
+++ net/can/af_can.c
@@ -288,8 +288,8 @@ int can_send(struct sk_buff *skb, int loop)
 		netif_rx_ni(newskb);
 
 	/* update statistics */
-	atomic_long_inc(&pkg_stats->tx_frames);
-	atomic_long_inc(&pkg_stats->tx_frames_delta);
+	pkg_stats->tx_frames++;
+	pkg_stats->tx_frames_delta++;
 
 	return 0;
 
@@ -659,8 +659,8 @@ static void can_receive(struct sk_buff *skb, struct net_device *dev)
 	int matches;
 
 	/* update statistics */
-	atomic_long_inc(&pkg_stats->rx_frames);
-	atomic_long_inc(&pkg_stats->rx_frames_delta);
+	pkg_stats->rx_frames++;
+	pkg_stats->rx_frames_delta++;
 
 	can_set_skb_uid(skb);
 
@@ -679,8 +679,8 @@ static void can_receive(struct sk_buff *skb, struct net_device *dev)
 	consume_skb(skb);
 
 	if (matches > 0) {
-		atomic_long_inc(&pkg_stats->matches);
-		atomic_long_inc(&pkg_stats->matches_delta);
+		pkg_stats->matches++;
+		pkg_stats->matches_delta++;
 	}
 }
 
